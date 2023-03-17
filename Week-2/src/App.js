09d0059c014cd5dfa7bb0d53350f7a6315d5b775
nitishpal <nitishpal99@gmail.com>
2023-03-18 00:19:30 +0530

    Third week commit

--- Week-2/src/App.js
+++ Week-2/src/App.js
@@ -1,11 +1,16 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Registration from "./components/Registration";
+import LoginForm from "./components/LoginForm";
 
 function App() {
   return (
     <div className="App">
       <Registration></Registration>
+      <br></br>
+      <br></br>
+      <br></br>
+      <LoginForm></LoginForm>
     </div>
   );
 }
